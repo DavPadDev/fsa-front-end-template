64882b86ffbda019782185190a00b6243192042c
dav.pad <dav.pad@davpaddev.com>
2023-09-04 18:11:55 -0500

    Completed template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackButtonComponent } from './components/back-button/back-button.component';
+import { SmallXComponent } from './components/small-x/small-x.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackButtonComponent,
+    SmallXComponent
   ],
   imports: [
     BrowserModule,
